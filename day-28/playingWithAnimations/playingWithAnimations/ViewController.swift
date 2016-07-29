fd9d3571fcef68b8dc1e4d0c0e0d2421c8146046
JennyShalai <jenny.shalai@gmail.com>
2016-07-29 14:33:20 -0400

    added upstream url

--- day-28/playingWithAnimations/playingWithAnimations/ViewController.swift
+++ day-28/playingWithAnimations/playingWithAnimations/ViewController.swift
@@ -15,19 +15,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // отключить ограничения супеп вида
         self.view.removeConstraints(self.view.constraints)
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        // отключить ограничения элемента
         self.redBox.removeConstraints(self.redBox.constraints)
         self.redBox.translatesAutoresizingMaskIntoConstraints=false
-
-        self.topConstrant =  self.redBox.topAnchor.constraintEqualToAnchor(self.view.centerYAnchor)
-        self.topConstrant?.active=true
-
-        self.redBox.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.25).active=true
-        self.redBox.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.25).active=true
-
+        // задать новые ограничения для элемента
+        // задание Х
         self.leftConstraint = self.redBox.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor)
-
         self.leftConstraint?.active=true
+        // задание Y
+        self.topConstrant = self.redBox.topAnchor.constraintEqualToAnchor(self.view.centerYAnchor)
+        self.topConstrant?.active=true
+        // задание высоты
+        self.redBox.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5).active = true
+        // задание ширины
+        self.redBox.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.45).active = true
+
+        
 
 
         // Do any additional setup after loading the view, typically from a nib.
@@ -44,17 +50,15 @@ class ViewController: UIViewController {
 
         let rightConstraint = self.redBox.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor)
 
-        UIView.animateWithDuration(0.1, delay: 0.0, options: [.Autoreverse], animations: {
+        UIView.animateWithDuration(1, delay: 0.0, options: [.Autoreverse], animations: {
             self.leftConstraint?.active=false
             rightConstraint.active=true
-
-
-            self.topConstrant?.constant=100
+            self.topConstrant?.constant = -100
             self.redBox.alpha = 0.25
             self.view.layoutIfNeeded()
         }) { (completed) in
             rightConstraint.active = false
-             self.leftConstraint?.active=true
+            self.leftConstraint?.active=true
             self.redBox.alpha=1.0
             self.topConstrant?.constant=0
             self.view.layoutIfNeeded()
